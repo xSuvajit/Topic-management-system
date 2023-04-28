1b97f53a195cbb1a1065108724236fcf8ccfe48d
suvajit97 <iamsuvajitsarkar@gmail.com>
2023-04-28 15:40:30 +0530

    Removing Useless Using

--- TMS.DTO/Topics.cs
+++ TMS.DTO/Topics.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TMS.DTO
+﻿namespace TMS.DTO
 {
     public class Topics
     {        
--- TMS.Manager/Contract/IUtilities.cs
+++ TMS.Manager/Contract/IUtilities.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TMS.DTO;
+﻿using TMS.DTO;
 
 namespace TMS.Manager.Contract
 {
